f9a32c00621ac639022a9eda6e032950b92a0b87
HE KYPTO <tsunyanapat@gmail.com>
2022-12-13 23:01:05 +0700

    PlayerTest Unrelated

---
+++ src/main/java/com/game/void_seekers/unrelated/TestPlayer.java
@@ -0,0 +1,10 @@
+package com.game.void_seekers.unrelated;
+
+import com.game.void_seekers.character.derived.PlayerJared;
+
+public class TestPlayer {
+    public static void main(String[] args) {
+        PlayerJared jared = new PlayerJared();
+        jared.displayHealthBar();
+    }
+}
--- src/test/java/com/game/void_seekers/TestPlayer.java
+++ src/test/java/com/game/void_seekers/TestPlayer.java
@@ -1,7 +1,31 @@
 package com.game.void_seekers;
 
 import com.game.void_seekers.character.base.PlayableCharacter;
+import com.game.void_seekers.character.derived.PlayerIsaac;
+import com.game.void_seekers.character.derived.PlayerJared;
+import com.game.void_seekers.character.derived.PlayerSoul;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPlayer {
-    PlayableCharacter
+    PlayableCharacter player1 = new PlayerIsaac();
+    PlayableCharacter player2 = new PlayerSoul();
+    PlayableCharacter player3 = new PlayerJared();
+
+    @BeforeEach
+    void setUp() {
+        player1 = new PlayerIsaac();
+        player2 = new PlayerSoul();
+        player3 = new PlayerJared();
+    }
+
+    @Test
+    void testCreatePlayer() {
+        player3.displayHealthBar();
+    }
+
+
+
 }
